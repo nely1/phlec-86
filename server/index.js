c8ea39a1088144e9408d13818c6d1b8be8c33621
nely1 <81120077+nely1@users.noreply.github.com>
2022-09-08 20:04:31 +1000

    added index file for data file

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import userRouter from './routes/userRouter.js';
+import data from './data/index.js';
 
 const app = express();
 dotenv.config();
@@ -14,13 +15,17 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewURLParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () =>console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+// mongoose.connect(process.env.CONNECTION_URL, {useNewURLParser: true, useUnifiedTopology: true})
+//     .then(() => app.listen(PORT, () =>console.log(`Server running on port: ${PORT}`)))
+//     .catch((error) => console.log(error.message));
 
+app.listen(process.env.PORT || 3000, () => {
+    console.log('phlecTravels is listening');
+})
 
 // Connect the router
 
 app.use('/userRouter', userRouter);
+
