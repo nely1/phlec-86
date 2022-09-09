3b40b7a80689ab264653bfb1d8624a66f02441c6
Chriissspy <phu00034@gmail.com>
2022-09-09 11:44:24 +1000

    drafted out album page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import LandingPageBody from './pages/LandingPageBody';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import HomePage from './pages/HomePage';
 import RecordPage from './pages/RecordPage';
+import AlbumPage from './pages/AlbumPage';
 
 
 
@@ -17,6 +17,7 @@ const HOME      = '/Home';
 const LOGOUT    = '/Logout';
 const SIGNUP    = '/SignUp';
 const RECORD    = '/Record';
+const ALBUM     = '/Album';
 
 
 export default function App() {
@@ -30,6 +31,7 @@ export default function App() {
                 <Route path={HOME} element={<HomePage />}></Route>
                 <Route path={SIGNUP} element={<SignUpPage />}></Route>
                 <Route path={RECORD} element={<RecordPage />}></Route>
+                <Route path={ALBUM} element={<AlbumPage />}></Route>
             </Routes> 
         </Router>
   )
