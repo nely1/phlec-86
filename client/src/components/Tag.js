f9ae3ee8d027261b495c09e3e8a3c1ba4f5d9c65
Chriissspy <phu00034@gmail.com>
2022-09-12 23:17:35 +1000

    created tag component

---
+++ client/src/components/Tag.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./Tag.css";
+export default function Tag() {
+  const [tags, setTags] = useState([]);
+
+  function handleEnter(e) {
+    if (e.key !== "Enter") return;
+    const value = e.target.value;
+    if (!value.trim()) return;
+    setTags([...tags, value]);
+    e.target.value = "";
+  }
+
+  function deleteTag(index) {
+    setTags(tags.filter((el, i) => i !== index));
+  }
+
+  return (
+    <div className="tagContainer">
+      {tags.map((tag, index) => (
+        <div className="tagItem" key={index}>
+          <span className="tagLabel text3">{tag}</span>
+          <span className="tagDelete" onClick={() => deleteTag(index)}>
+            &times;
+          </span>
+        </div>
+      ))}
+      <input
+        className="tagInput"
+        type="text"
+        placeholder="Enter you tag"
+        onKeyDown={handleEnter}
+      ></input>
+    </div>
+  );
+}
--- client/src/pages/RecordPage.js
+++ client/src/pages/RecordPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Tag from '../components/Tag';
 import './RecordPage.css'
 export default function RecordPage() {
     const [mainImage] = useState(() => { return 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'});
@@ -17,7 +18,8 @@ export default function RecordPage() {
                 <input className='RecordPageAlbumNameInput'type="text" placeholder=' Name...'></input>
                 <h3 className='RecordPageDescription'>Description</h3>
                 <textarea className='RecordPageDescriptionInput' placeholder=' Type Your Description Here'></textarea>
-                <h3 className='RecordPageTagTitle'>Tags:</h3>
+                <h3 className='RecordPageTagTitle'>Tags</h3>
+                <Tag></Tag>
                 <div className='RecordPageSave'>
                     <p className='text2'>Save</p>
                 </div>
