69e597cf20732429ac2b57770c378b444205ff60
Chriissspy <phu00034@gmail.com>
2022-09-16 11:35:12 +1000

    change album page to take an array instead of hard code, create a albumPlaceBox componenet

--- client/src/pages/AlbumPage.js
+++ client/src/pages/AlbumPage.js
@@ -1,38 +1,48 @@
-import React from 'react'
-import './AlbumPage.css'
+import React from "react";
+import AlbumPlaceBox from "../components/AlbumPlaceBox";
+import "./AlbumPage.css";
 export default function AlbumPage() {
-  return (
-    <>
-        <div className='AlbumPageParameter'>
-            <h2>Filter:</h2>
-            <h2>Tags:</h2>
-            <input className='AlbumPageSearch' placeholder='Search...'></input>
-        </div>
-        
-      {/* This would be a good candidate for a component, and should probably
-        * put them in a specific grid/flexbox so that they are more responsive. */}
-        <div className='AlbumPageGrid'>
-            <div className='AlbumPageItems'>
-                <div className='tmpBox'></div>
-                <h3>PlaceName</h3>
-                <p className='text3'>Date & Time + Number of Photos</p>
+    const albums = [
+        {
+            place: "Melbourne",
+            date: "01/02/2022",
+            numberPhotos: 2,
+            image: {
+                url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Melburnian_Skyline.jpg/1200px-Melburnian_Skyline.jpg",
+                alt: "one",
+            },
+        },
+        {
+            place: "Sydney",
+            date: "09/04/1999",
+            numberPhotos: 20,
+            image: {
+                url: "https://www.sydney.com/sites/sydney/files/styles/open_graph/public/2022-02/164142-Sydney%20Harbour%20-%20sunrise%20over%20Circular%20Quay.jpg?itok=AKlqtCiC",
+                alt: "",
+            },
+        },
+    ];
+    return (
+        <>
+            <div className="AlbumPageParameter">
+                <h2>Filter:</h2>
+                <h2>Tags:</h2>
+                <input
+                    className="AlbumPageSearch"
+                    placeholder="Search..."
+                ></input>
             </div>
-            <div className='AlbumPageItems'>
-                <div className='tmpBox'></div>
-                <h3>PlaceName</h3>
-                <p className='text3'>Date & Time + Number of Photos</p>
+
+            {/* This would be a good candidate for a component, and should probably
+             * put them in a specific grid/flexbox so that they are more responsive. */}
+            <div className="AlbumPageGrid">
+                {albums.map((album, index) => (
+                    <AlbumPlaceBox
+                        key={index}
+                        album={albums[index]}
+                    ></AlbumPlaceBox>
+                ))}
             </div>
-            <div className='AlbumPageItems'>
-                <div className='tmpBox'></div>
-                <h3>PlaceName</h3>
-                <p className='text3'>Date & Time + Number of Photos</p>
-            </div>
-            <div className='AlbumPageItems'>
-                <div className='tmpBox'></div>
-                <h3>PlaceName</h3>
-                <p className='text3'>Date & Time + Number of Photos</p>
-            </div>
-        </div>
-    </>
-  )
+        </>
+    );
 }
