3b7dbc3d02ecf8c55d4efecf333369e53468f031
Chriissspy <phu00034@gmail.com>
2022-09-22 10:32:32 +1000

    show albums that are only created and belong to user

--- server/controllers/albumController.js
+++ server/controllers/albumController.js
@@ -1,15 +1,13 @@
 import album from "../data/albumModel.js";
 
 const display = async (req, res) => {
-
     try {
-        console.log(req.userId);
-        const albums = await album.find();
+        const userId = req.params.id;
+        const albums = await album.find({ userid: userId });
         res.status(200).json(albums);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
 };
-
 
 export default { display };
