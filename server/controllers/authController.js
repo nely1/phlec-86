c32c13e5a49fc6f70dd4445df41d0905f50eeec5
Chriissspy <phu00034@gmail.com>
2022-09-24 22:12:27 +1000

    merged with albums branch v2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,35 +1,27 @@
-import jwt from 'jsonwebtoken';
-import User from '../data/userModel.js' 
+import jwt from "jsonwebtoken";
+import User from "../data/userModel.js";
 
-  
-export const loginUser = async (req,res) => {
+export const loginUser = async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
     User.findOne({ email }, {}, {}, (err, user) => {
         console.log("Successfully reached authentication");
-        
+
         if (err) {
-            res.status(500).json({message: 'Unknown error occured, please try again later'});
-        }
-
-        else if (!user) {
-            res.status(404).json({message: 'Incorrect email or password'});
-        }
-
-        else if (!user.verifyPassword(password)){
-            res.status(404).json({message: 'Incorrect email or password'});
-        }
-        else{
-            
-            const token = jwt.sign({email: user.email, id: user._id}, "super secret stuff", {expiresIn: "1h"});
-            res.status(200).json({result: user, token});
+            res.status(500).json({ message: "Unknown error occured, please try again later" });
+        } else if (!user) {
+            res.status(404).json({ message: "Incorrect email or password" });
+        } else if (!user.verifyPassword(password)) {
+            res.status(404).json({ message: "Incorrect email or password" });
+        } else {
+            const token = jwt.sign({ email: user.email, id: user._id }, "super secret stuff", { expiresIn: "1h" });
+            res.status(200).json({ result: user, token });
 
             console.log("Somewhat logged in");
-           
         }
-     })
-    }
+    });
+};
 
 export const signUpUser = async (req, res) => {
     const firstName = req.body.firstName;
@@ -38,46 +30,32 @@ export const signUpUser = async (req, res) => {
     const userName = req.body.userName;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
-    const userName = req.body.userName;
-
-
 
     try {
-        const user = await User.findOne({email});
+        const user = await User.findOne({ email });
 
         console.log("Successfully reached authentication");
-        
-        
 
         if (user) {
-            res.status(404).json({message: 'User already exists'});
-        }
-
-        else if (password !== confirmPassword){
-            res.status(404).json({message: 'Passwords do not match'});
-        }
-        else{
-            
+            res.status(404).json({ message: "User already exists" });
+        } else if (password !== confirmPassword) {
+            res.status(404).json({ message: "Passwords do not match" });
+        } else {
             const newUser = await User.create({
-                firstName: firstName, 
-                lastName: lastName, 
-                email: email, 
-                password: password, 
-                favourites: [], 
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password,
+                favourites: [],
                 userName: userName,
-                plans: []});
+                plans: [],
+            });
 
-            
-
-            res.status(200).json({message: 'Account created',newUser});   
+            res.status(200).json({ message: "Account created", newUser });
             console.log("account created");
-           
         }
-    }
-    catch (error) {
-        res.status(500).json({message: 'Unknown error occured, please try again later'});
+    } catch (error) {
+        res.status(500).json({ message: "Unknown error occured, please try again later" });
         console.log(error.message);
     }
-    
-}
-
+};
