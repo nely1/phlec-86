f0c4fd85fa9c110ce80274910022120aebaf005a
apple <zhengyang.hong@student.unimelb.edu.au>
2022-09-26 21:15:27 +1000

    Adjust the indentation so as to follow the coding standard

--- client/src/pages/SettingsPage.js
+++ client/src/pages/SettingsPage.js
@@ -9,77 +9,75 @@ function SettingsPage() {
     const userInfo = JSON.parse(localStorage.getItem("profile"));
 
     function changingState() {
-      setChange((prev) => !prev);
+        setChange((prev) => !prev);
     }
 
     function handleChange(event) {
-      changingState();
+        changingState();
     }
 
     return (
-    <>
-    <div className='SettingsPage'>
-      <div className='SettingsPageBox'>
-        
-          <img className="profileImage2" src="profiledefault.png" alt="Default Profile" />
-          {!change ? (
-            <>
-              <p className='PersonalInfo'>
-                Name : &emsp;&emsp;&emsp; {userInfo.result.firstName + " " + userInfo.result.lastName}
-              </p>         
-              <p className='PersonalInfo'>
-                Username : &emsp; {userInfo.result.userName}
-              </p>            
-              <p className='PersonalInfo'>
-                Email : &emsp;&emsp;&emsp; {userInfo.result.email}    
-              </p>           
-              <p className='PersonalInfo'>
-                Theme : &emsp;&emsp;&ensp; {userInfo.result.theme}
-              </p>
-              <p className='PersonalInfo'>
-                Password : 
-              </p>           
-              <button className='ChangeButton text3' onClick={changingState}> Change </button>
-            </>
-          ) : (
-            <>
-              <p className='PersonalInfo'> 
-                Name : &emsp;&emsp;&emsp;
-                <label className = 'SettingsPageInputBox'>                
-                  <input className='SettingsPageInputField' type='text'></input>
-                </label>   
-              </p>   
-              <p className='PersonalInfo'>
-                Username : &emsp;
-                <label className = 'SettingsPageInputBox'>                
-                  <input className='SettingsPageInputField' type='text'></input>
-                </label>
-              </p>            
-              <p className='PersonalInfo'>
-                Email : &emsp;&emsp;&emsp;
-                <label className = 'SettingsPageInputBox'>                
-                  <input className='SettingsPageInputField' type='text'></input>
-                </label>    
-              </p>           
-              <p className='PersonalInfo'>
-                Theme : &emsp;&emsp;&ensp;
-                <label className = 'SettingsPageInputBox'>                
-                  <input className='SettingsPageInputField' type='text'></input>
-                </label>
-              </p>
-              <p className='PersonalInfo'>
-                Password : &emsp;&nbsp;
-                <label className = 'SettingsPageInputBox'>                
-                  <input className='SettingsPageInputField' type='password'></input>
-                </label>
-              </p>           
-              <button className='ChangeButton text3' onClick={handleChange}> Save </button>
-            </>
-          ) }
-        
-      </div>
-    </div>
-    </>
+        <>
+            <div className='SettingsPage'>
+                <div className='SettingsPageBox'>       
+                    <img className="profileImage2" src="profiledefault.png" alt="Default Profile" />
+                    {!change ? (
+                        <>
+                            <p className='PersonalInfo'>
+                                Name : &emsp;&emsp;&emsp; {userInfo.result.firstName + " " + userInfo.result.lastName}
+                            </p>         
+                            <p className='PersonalInfo'>
+                                Username : &emsp; {userInfo.result.userName}
+                            </p>            
+                            <p className='PersonalInfo'>
+                                Email : &emsp;&emsp;&emsp; {userInfo.result.email}    
+                            </p>           
+                            <p className='PersonalInfo'>
+                                Theme : &emsp;&emsp;&ensp; {userInfo.result.theme}
+                            </p>
+                            <p className='PersonalInfo'>
+                                Password : 
+                            </p>           
+                            <button className='ChangeButton text3' onClick={changingState}> Change </button>
+                        </>
+                    ) : (
+                        <>
+                            <p className='PersonalInfo'> 
+                                Name : &emsp;&emsp;&emsp;
+                                <label className = 'SettingsPageInputBox'>                
+                                    <input className='SettingsPageInputField' type='text'></input>
+                                </label>   
+                            </p>   
+                            <p className='PersonalInfo'>
+                                Username : &emsp;
+                                <label className = 'SettingsPageInputBox'>                
+                                    <input className='SettingsPageInputField' type='text'></input>
+                                </label>
+                            </p>            
+                            <p className='PersonalInfo'>
+                                Email : &emsp;&emsp;&emsp;
+                                <label className = 'SettingsPageInputBox'>                
+                                    <input className='SettingsPageInputField' type='text'></input>
+                                </label>    
+                            </p>           
+                            <p className='PersonalInfo'>
+                                Theme : &emsp;&emsp;&ensp;
+                                <label className = 'SettingsPageInputBox'>                
+                                    <input className='SettingsPageInputField' type='text'></input>
+                                </label>
+                            </p>
+                            <p className='PersonalInfo'>
+                                Password : &emsp;&nbsp;
+                                <label className = 'SettingsPageInputBox'>                
+                                    <input className='SettingsPageInputField' type='password'></input>
+                                </label>
+                            </p>           
+                            <button className='ChangeButton text3' onClick={handleChange}> Save </button>
+                        </>
+                    ) }  
+                </div>
+            </div>
+        </>
     )
 }
 
