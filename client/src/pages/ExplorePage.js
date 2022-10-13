64f5823080d032495e76ab984cd75a901559c261
Mirtekore <engjiaee@gmail.com>
2022-10-14 00:37:08 +1100

    Added fix for array mutability

--- client/src/pages/ExplorePage.js
+++ client/src/pages/ExplorePage.js
@@ -1,100 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getLocations } from "../actions/location";
-
-import "./ExplorePage.css";
-
-import SearchBar from "../components/SearchBar";
-import ExploreCard from "../components/ExploreCard";
-import ReviewCard from "../components/ReviewCard";
-import { useNavigate } from "react-router-dom";
-
-
-
-export default function ExplorePage({loginState}) {
-
-    const [selected, setSelected] = useState(0);
-
-    const locations = useSelector((state) => state?.location);
-    const dispatch = useDispatch();
-    const history = useNavigate();
-
-    
-    useEffect(() => {
-        if (!loginState) {
-            history("/login");
-        }
-        else {
-            dispatch(getLocations());
-        }
-    }, [history, loginState, dispatch]);
-
-    
-
-    let locationArray = [];
-    let reviews;
-
-    // convert data to array
-    locations.map((loc) => {locationArray.push(loc)})
-    if (locationArray.length > 0) {
-        reviews = locationArray[selected].reviews; 
-        }
-
-    
-    console.log(reviews);
-    for (let i = 0; i < locations.length; i++) {
-        locationArray[i] = 
-            <li key={i} onClick={() => { setSelected(i); reviews = locationArray[selected].reviews }}>
-                <ExploreCard selected={selected == i ? true : false} data={ locationArray[i] } />
-            </li>;
-    }
-
-    if (reviews !== undefined) {
-        reviews.map((review) => {<li><ReviewCard data={review} /></li>})
-    }
-
-    function isSelected(i) {
-        if (selected == i) {
-            return true;
-        } else {
-            return false
-        }
-    }
-
-    if (!loginState) {
-        return <></>;
-    }
-
-    return (
-        <div>
-            <div className="exploreFilters">
-                <SearchBar /> 
-            </div>
-            <div className="Explore-Reviews">
-                <ul className="exploreCards" >
-                    {locationArray}
-                </ul>
-                <div className="reviewCards">
-                    <ul className="reviews">
-                        { reviews === undefined ? <div></div> : reviews }
-                        
-                    </ul>
-                    {/* if (loggedIn && ! user has review) */}
-                    <div className="reviewCard userReview">
-                        <form className="userReview">
-                            <textarea className="userReviewContent"/>
-                        <input className="styledButton untoggledButton" type="submit" value="Post"/>
-                        </form>
-                    </div>
-                </div>
-            </div>            
-        </div>
-    );
-}
-
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getLocations } from "../actions/location";
 import { postReview } from "../actions/explore";
 
 import "./ExplorePage.css";
@@ -104,22 +10,16 @@ import ExploreCard from "../components/ExploreCard";
 import ReviewCard from "../components/ReviewCard";
 import { useNavigate } from "react-router-dom";
 
-
-
 export default function ExplorePage({loginState}) {
 
-    const [selected, setSelected] = useState(0);
-    const [review, setReview] = useState("");
-    const locations = useSelector((state) => state.location);
     const dispatch = useDispatch();
     const history = useNavigate();
-
-    
+    const [selected, setSelected] = useState(0);
+    const locations = useSelector((state) => state?.location);
     useEffect(() => {
         if (!loginState) {
             history("/login");
-        }
-        
+        }       
     }, [history, loginState]);
     
     useEffect(() => {
@@ -140,12 +40,11 @@ export default function ExplorePage({loginState}) {
     let locationArray = [];
     let reviews;
 
-    
     // convert data to array
-    locationArray = locations;
+    locationArray = locations.slice(0);
     if (locationArray.length > 0) {
         reviews = locationArray[selected].reviews; 
-        }
+    }
 
     
     console.log(reviews);
@@ -167,6 +66,7 @@ export default function ExplorePage({loginState}) {
             return false
         }
     }
+
 
     if (!loginState) {
         return <></>;
