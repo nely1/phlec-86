7d6b98a61f5bcc3f1e6131d44ae68816e524769e
fchats <fchataway@student.unimelb.edu.au>
2022-10-22 15:47:44 +1100

    prepared location and review models for db review update

--- server/data/reviewModel.js
+++ server/data/reviewModel.js
@@ -2,10 +2,9 @@ import mongoose from 'mongoose';
 
 const schema = new mongoose.Schema({ 
     description: String,
-    // rating: {type: Number, required: true},
     dateUploaded: Date,
-    locationid: {type: mongoose.Schema.Types.ObjectId, ref: 'locationModel'},
-    userid: {type: mongoose.Schema.Types.ObjectId, ref: 'usermodel'},
+    locationId: {type: mongoose.Schema.Types.ObjectId, ref: 'locationModel'},
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'usermodel'},
 }) 
 
 const reviewModel = mongoose.model('reviewModel', schema);
